5140a39ff9350d1047e2d170e296a4eccbf838b7
Filip Trela <ftrela@gmail.com>
2019-07-15 19:27:24 +0200

    commit after make a NBP API

---
+++ src/main/java/pl/sda/gdajava25/CurrencyCode.java
@@ -0,0 +1,23 @@
+package pl.sda.gdajava25;
+
+public enum CurrencyCode {
+    USD,
+    GBP,
+    EUR,
+    CHF,
+    CNY,
+    ZWL,
+    AUD,
+    BGN,
+    BRL,
+    CAD,
+    CLF,
+    CLP,
+    COP,
+    CZK,
+    IDR,
+    JPY,
+    MXN,
+    NOK,
+    THB
+}
